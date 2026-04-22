fc08b669f82a4fa3cfde8606c70d55cfdc01dc6a
F88 <685250+F88@users.noreply.github.com>
2026-04-22 14:48:34 +0900

    feat(layout): add responsive map sheet sizing

--- src/components/map-bottom-sheet-layout.tsx
+++ src/components/map-bottom-sheet-layout.tsx
@@ -1,11 +1,11 @@
-import { useState, type ReactNode } from 'react';
+import { useEffect, useState, type ReactNode } from 'react';
 import { BottomSheet, type BottomSheetProps } from './bottom-sheet';
 import { MapView, type MapViewProps } from './map/map-view';
+import { useViewportHeight } from '../hooks/use-viewport-height';
+import { resolveMapBottomSheetLayoutPreset } from '../utils/map-bottom-sheet-layout-preset';
+import { createLogger } from '../lib/logger';
 
-const COLLAPSED_MAP_HEIGHT_CLASS = 'h-[60dvh]';
-const EXPANDED_MAP_HEIGHT_CLASS = 'h-[60dvh]';
-const COLLAPSED_SHEET_HEIGHT_CLASS = 'h-[40dvh]';
-const EXPANDED_SHEET_HEIGHT_CLASS = 'h-[70dvh]';
+const logger = createLogger('MapBottomSheetLayout');
 
 interface MapBottomSheetLayoutProps {
   mapViewProps: MapViewProps;
@@ -19,13 +19,25 @@ export function MapBottomSheetLayout({
   mapOverlay,
 }: MapBottomSheetLayoutProps) {
   const [expanded, setExpanded] = useState(false);
+  const viewportHeight = useViewportHeight();
+  const layoutPreset = resolveMapBottomSheetLayoutPreset(viewportHeight);
+
+  useEffect(() => {
+    logger.debug(
+      `viewportHeight=${viewportHeight}, collapsedMap=${layoutPreset.collapsedMapHeightClassName}, expandedMap=${layoutPreset.expandedMapHeightClassName}, collapsedSheet=${layoutPreset.collapsedSheetHeightClassName}, expandedSheet=${layoutPreset.expandedSheetHeightClassName}`,
+    );
+  }, [layoutPreset, viewportHeight]);
 
   return (
     <>
       <div className="relative">
         <MapView
           {...mapViewProps}
-          heightClassName={expanded ? EXPANDED_MAP_HEIGHT_CLASS : COLLAPSED_MAP_HEIGHT_CLASS}
+          heightClassName={
+            expanded
+              ? layoutPreset.expandedMapHeightClassName
+              : layoutPreset.collapsedMapHeightClassName
+          }
         />
         {mapOverlay}
       </div>
@@ -33,8 +45,8 @@ export function MapBottomSheetLayout({
         {...bottomSheetProps}
         expanded={expanded}
         onExpandedChange={setExpanded}
-        collapsedHeightClassName={COLLAPSED_SHEET_HEIGHT_CLASS}
-        expandedHeightClassName={EXPANDED_SHEET_HEIGHT_CLASS}
+        collapsedHeightClassName={layoutPreset.collapsedSheetHeightClassName}
+        expandedHeightClassName={layoutPreset.expandedSheetHeightClassName}
       />
     </>
   );
