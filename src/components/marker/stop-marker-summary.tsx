7c9dc7db43aa4746e9f99517c537bc4208ed8a12
F88 <685250+F88@users.noreply.github.com>
2026-05-29 18:42:37 +0900

    refactor(marker): format PlatformCodeLabel rendering for better readability

--- src/components/marker/stop-marker-summary.tsx
+++ src/components/marker/stop-marker-summary.tsx
@@ -64,7 +64,14 @@ export function StopMarkerSummary({
         <span>
           {routeTypesEmoji(routeTypes)} {stopNames.name}
         </span>
-        {stop.platform_code && <PlatformCodeLabel code={stop.platform_code} size="xs" className="font-normal" />}
+        {stop.platform_code && (
+          <PlatformCodeLabel
+            //
+            code={stop.platform_code}
+            size="xs"
+            className="font-normal"
+          />
+        )}
         {agencies.length > 0 &&
           agencies.map((a) => (
             <AgencyBadge
